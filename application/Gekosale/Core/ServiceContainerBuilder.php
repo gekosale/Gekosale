d6d6c67eb5fe9b253336e4ca39c5278473ff051e
Adam Piotrowski <apiotrowski@wellcommerce.pl>
2014-02-12 23:07:38 +0100

    Formatowanie

--- application/Gekosale/Core/ServiceContainerBuilder.php
+++ application/Gekosale/Core/ServiceContainerBuilder.php
@@ -34,186 +34,183 @@ use Gekosale\Core\DependencyInjection\Extension\PluginExtensionLoader;
 final class ServiceContainerBuilder
 {
 
-	/**
+    /**
 	 * Cached container class name
 	 *
 	 * @var string
 	 */
-	const SERVICE_CONTAINER_CLASS = 'ServiceContainer';
+    const SERVICE_CONTAINER_CLASS = 'ServiceContainer';
 
-	/**
+    /**
 	 * Cached container parent class name
 	 *
 	 * @var string
 	 */
-	const SERVICE_CONTAINER_BASE_CLASS = 'Container';
+    const SERVICE_CONTAINER_BASE_CLASS = 'Container';
 
-	/**
+    /**
 	 * Container builder instance
 	 *
 	 * @var object
 	 */
-	protected $containerBuilder;
+    protected $containerBuilder;
 
-	/**
+    /**
 	 * Array containing default kernel parameters used in configuring services
 	 *
 	 * @var array
 	 */
-	protected $parameters;
+    protected $parameters;
 
-	/**
+    /**
 	 * True if the debug mode is enabled, false otherwise
 	 *
 	 * @var bool
 	 */
-	protected $isDebug;
+    protected $isDebug;
 
-	/**
+    /**
 	 * Compiler passes registered to run during container build process
 	 *
 	 * @var array
 	 */
-	protected $compilerPasses;
+    protected $compilerPasses;
 
-	/**
+    /**
 	 * Path to cached container class
 	 *
 	 * @var string
 	 */
-	protected $compilerClassPath;
+    protected $compilerClassPath;
 
-	/**
+    /**
 	 * Config instance class
 	 *
 	 * @var ConfigCache
 	 */
-	protected $containerConfigCache;
+    protected $containerConfigCache;
 
-	/**
+    /**
 	 * Constructor
 	 *
 	 * @param array $parameters
 	 * @param bool  $isDebug
 	 */
-	public function __construct(array $parameters, $isDebug = FALSE)
-	{
-		$this->parameters           = $parameters;
-		$this->isDebug              = (bool)$isDebug;
-		$this->compilerClassPath    = __DIR__ . DS . self::SERVICE_CONTAINER_CLASS . '.php';
-		$this->containerConfigCache = new ConfigCache($this->compilerClassPath, $this->isDebug);
-	}
+    public function __construct (array $parameters, $isDebug = FALSE)
+    {
+        $this->parameters = $parameters;
+        $this->isDebug = (bool) $isDebug;
+        $this->compilerClassPath = __DIR__ . DS . self::SERVICE_CONTAINER_CLASS . '.php';
+        $this->containerConfigCache = new ConfigCache($this->compilerClassPath, $this->isDebug);
+    }
 
-	/**
+    /**
 	 * Checks if optimised container class needs to be regenerated
 	 *
 	 * @return void
 	 */
+    public function check ()
+    {
+        if (! $this->containerConfigCache->isFresh()) {
+            
+            $this->initContainerBuilder();
+            
+            $this->loadXmlConfiguration();
+            
+            $this->registerExtensions();
+            
+            $this->registerCompilerPasses();
+            
+            foreach ($this->compilerPasses as $compilerPass) {
+                $compilerPass->process($this->containerBuilder);
+            }
+            
+            $this->compileAndSaveContainer();
+        }
+    }
 
-	public function check()
-	{
-		if (!$this->containerConfigCache->isFresh()) {
-
-			$this->initContainerBuilder();
-
-			$this->loadXmlConfiguration();
-
-			$this->registerExtensions();
-
-			$this->registerCompilerPasses();
-
-			foreach ($this->compilerPasses as $compilerPass) {
-				$compilerPass->process($this->containerBuilder);
-			}
-
-			$this->compileAndSaveContainer();
-		}
-	}
-
-	/**
+    /**
 	 * Register extensions using recursive scan
 	 * 
 	 * @return void
 	 */
-	protected function registerExtensions()
-	{
-		$extensionLoader = new PluginExtensionLoader($this->containerBuilder);
-		$extensionLoader->registerExtensions();
-	}
+    protected function registerExtensions ()
+    {
+        $extensionLoader = new PluginExtensionLoader($this->containerBuilder);
+        $extensionLoader->registerExtensions();
+    }
 
-	/**
+    /**
 	 * Compiles container and saves it as an optimized class
 	 *
 	 * @return void
 	 */
-	protected function compileAndSaveContainer()
-	{
-		$this->containerBuilder->compile();
+    protected function compileAndSaveContainer ()
+    {
+        $this->containerBuilder->compile();
+        
+        $dumper = new PhpDumper($this->containerBuilder);
+        
+        $content = $dumper->dump([
+            'class' => self::SERVICE_CONTAINER_CLASS,
+            'base_class' => self::SERVICE_CONTAINER_BASE_CLASS,
+            'namespace' => __NAMESPACE__
+        ]);
+        
+        $this->containerConfigCache->write($content, $this->containerBuilder->getResources());
+    }
 
-		$dumper = new PhpDumper($this->containerBuilder);
-
-		$content = $dumper->dump(
-		                  [
-		                  'class'      => self::SERVICE_CONTAINER_CLASS,
-		                  'base_class' => self::SERVICE_CONTAINER_BASE_CLASS,
-		                  'namespace'  => __NAMESPACE__
-		                  ]
-		);
-
-		$this->containerConfigCache->write($content, $this->containerBuilder->getResources());
-	}
-
-	/**
+    /**
 	 * Initializes Container Builder instance
 	 *
 	 * @return void
 	 */
-	protected function initContainerBuilder()
-	{
-		$parameterBag           = new ParameterBag($this->parameters);
-		$this->containerBuilder = new ContainerBuilder($parameterBag);
-	}
+    protected function initContainerBuilder ()
+    {
+        $parameterBag = new ParameterBag($this->parameters);
+        $this->containerBuilder = new ContainerBuilder($parameterBag);
+    }
 
-	/**
+    /**
 	 * Loads services configuration and predefined parameters from XML config file
 	 *
 	 * @return void
 	 */
-	protected function loadXmlConfiguration()
-	{
-		$locator = new FileLocator(ROOTPATH . 'config');
+    protected function loadXmlConfiguration ()
+    {
+        $locator = new FileLocator(ROOTPATH . 'config');
+        
+        $loader = new XmlFileLoader($this->containerBuilder, $locator);
+        $loader->load('config.xml');
+    }
 
-		$loader = new XmlFileLoader($this->containerBuilder, $locator);
-		$loader->load('config.xml');
-	}
-
-	/**
+    /**
 	 * Registers Compiler passess used in process of compiling the container
 	 *
 	 * @param CompilerPassInterface $compilerPass
 	 *
 	 * @return void
 	 */
-	protected function registerCompilerPass(CompilerPassInterface $compilerPass)
-	{
-		$this->compilerPasses[] = $compilerPass;
-	}
+    protected function registerCompilerPass (CompilerPassInterface $compilerPass)
+    {
+        $this->compilerPasses[] = $compilerPass;
+    }
 
-	/**
+    /**
 	 * Register all required compiler passes
 	 *
 	 * @return void
 	 */
-	protected function registerCompilerPasses()
-	{
-		/*
+    protected function registerCompilerPasses ()
+    {
+        /*
 		 * RegisterListenersPass for all event listeners
 		 */
-		$this->registerCompilerPass(new RegisterListenersPass());
-
-		/*
+        $this->registerCompilerPass(new RegisterListenersPass());
+        
+        /*
 		 * RegisterTwigExtensionsPass for all services tagged with twig.extension
 		 */
-		$this->registerCompilerPass(new RegisterTwigExtensionsPass());
-	}
+        $this->registerCompilerPass(new RegisterTwigExtensionsPass());
+    }
 }
