91124ea0064ec7742ad2c5fecbe6e3318a14ccd1
Adam Piotrowski <adam@gekosale.com>
2014-02-26 22:13:28 +0100

    Brackets

--- application/Gekosale/Plugin/Currency/Form/CurrencyForm.php
+++ application/Gekosale/Plugin/Currency/Form/CurrencyForm.php
@@ -26,72 +26,72 @@ class CurrencyForm extends Form
 
     public function init($currencyData = Array())
     {
-        $form = new FormEngine\Elements\Form(Array(
+        $form = new FormEngine\Elements\Form([
             'name' => 'currency',
-        ));
+        ]);
 
-        $requiredData = $form->AddChild(new FormEngine\Elements\Fieldset(Array(
+        $requiredData = $form->AddChild(new FormEngine\Elements\Fieldset([
             'name'  => 'required_data',
             'label' => $this->trans('Basic information')
-        )));
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'name',
             'label' => $this->trans('Name'),
-            'rules' => Array(
+            'rules' => [
                 new FormEngine\Rules\Required($this->trans('Name is required'))
-            )
-        )));
+            ]
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\Select(Array(
+        $requiredData->AddChild(new FormEngine\Elements\Select([
             'name'    => 'symbol',
             'label'   => $this->trans('Symbol'),
             'options' => FormEngine\Option::Make($this->get('currency.repository')->getCurrencySymbols()),
-            'rules'   => Array(
+            'rules'   => [
                 new FormEngine\Rules\Required($this->trans('Symbol is required'))
-            )
-        )));
+            ]
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'decimal_separator',
             'label' => $this->trans('Decimal separator'),
-            'rules' => Array(
+            'rules' => [
                 new FormEngine\Rules\Required($this->trans('Decimal separator is required'))
-            )
-        )));
+            ]
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'decimal_count',
             'label' => $this->trans('Decimal count'),
-            'rules' => Array(
+            'rules' => [
                 new FormEngine\Rules\Required($this->trans('Decimal count is required'))
-            )
-        )));
+            ]
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'thousand_separator',
             'label' => $this->trans('Thousands separator')
-        )));
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'positive_prefix',
             'label' => $this->trans('Positive prefix')
-        )));
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'positive_sufix',
             'label' => $this->trans('Positive sufix')
-        )));
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'negative_prefix',
             'label' => $this->trans('Negative prefix')
-        )));
+        ]));
 
-        $requiredData->AddChild(new FormEngine\Elements\TextField(Array(
+        $requiredData->AddChild(new FormEngine\Elements\TextField([
             'name'  => 'negative_sufix',
             'label' => $this->trans('Negative sufix')
-        )));
+        ]));
 
         $form->AddFilter(new FormEngine\Filters\NoCode());
 
