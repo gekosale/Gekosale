05abd55f6af24662280477eac9ed9a30fdd678ef
Adam Piotrowski <adam@gekosale.com>
2014-03-22 13:21:37 +0100

    TaxDataGrid

--- application/Gekosale/Plugin/Tax/DataGrid/TaxDataGrid.php
+++ application/Gekosale/Plugin/Tax/DataGrid/TaxDataGrid.php
@@ -11,8 +11,9 @@
  */
 namespace Gekosale\Plugin\Tax\DataGrid;
 
-use Gekosale\Core\DataGrid,
-    Gekosale\Core\DataGrid\DataGridInterface;
+use Gekosale\Core\DataGrid;
+use Gekosale\Core\DataGrid\DataGridInterface;
+use Gekosale\Plugin\Tax\Event\TaxDataGridEvent;
 
 /**
  * Class TaxDataGrid
@@ -25,32 +26,61 @@ class TaxDataGrid extends DataGrid implements DataGridInterface
     /**
      * {@inheritdoc}
      */
+    public function configure()
+    {
+        $this->setOptions([
+            'id'             => 'tax',
+            'event_handlers' => [
+                'load'       => $this->getXajaxManager()->registerFunction(['LoadTax', $this, 'loadData']),
+                'edit_row'   => 'editTax',
+                'click_row'  => 'editTax',
+                'delete_row' => $this->getXajaxManager()->registerFunction(['DeleteTax', $this, 'deleteRow'])
+            ],
+            'routes'         => [
+                'edit' => $this->generateUrl('admin.tax.edit')
+            ]
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function init()
     {
-        $this->registerEventHandlers();
-
         $this->addColumn('id', [
-            'source' => 'tax.id'
+            'source'     => 'tax.id',
+            'caption'    => $this->trans('Id'),
+            'sorting'    => [
+                'default_order' => DataGridInterface::SORT_DIR_DESC
+            ],
+            'appearance' => [
+                'width'   => 90,
+                'visible' => false
+            ],
+            'filter'     => [
+                'type' => DataGridInterface::FILTER_BETWEEN
+            ]
         ]);
 
         $this->addColumn('name', [
-            'source' => 'tax_translation.name'
+            'source'     => 'tax_translation.name',
+            'caption'    => $this->trans('Name'),
+            'appearance' => [
+                'width' => 70,
+                'align' => DataGridInterface::ALIGN_LEFT
+            ],
+            'filter'     => [
+                'type' => DataGridInterface::FILTER_INPUT
+            ]
         ]);
 
         $this->query = $this->getDb()
             ->table('tax')
             ->join('tax_translation', 'tax_translation.tax_id', '=', 'tax.id')
             ->groupBy('tax.id');
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function registerEventHandlers()
-    {
-        $this->getXajaxManager()->registerFunctions([
-            'getTaxForAjax' => [$this, 'getData'],
-            'doDeleteTax'   => [$this, 'delete']
-        ]);
+        $event = new TaxDataGridEvent($this);
+
+        $this->getDispatcher()->dispatch(TaxDataGridEvent::DATAGRID_INIT_EVENT, $event);
     }
 }
